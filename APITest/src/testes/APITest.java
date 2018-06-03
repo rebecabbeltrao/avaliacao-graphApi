df6f4cd4fc3774de0eb5e3b5a3a4b149086b9c8c
rebecabbeltrao <rebeca.oliveira@ccc.ufcg.edu.br>
2018-06-03 20:48:47 -0300

    testes connected, vertex, edge e mean edge

--- APITest/src/testes/APITest.java
+++ APITest/src/testes/APITest.java
@@ -1,6 +1,8 @@
 package testes;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import java.io.IOException;
 
@@ -16,14 +18,20 @@ public class APITest {
 	private static Graph grafo2;
 	private static Graph grafo3;
 	private static Graph grafo4;
+	private static Graph grafoTeste1;
+	private static Graph grafoTeste2;
+	private static Graph grafoTeste3;
+	private static Graph grafoTeste4;
+	private static Graph grafoTeste5;
+	private static Graph grafoTeste6;
+	private static Graph grafoTeste7;
 	private Graph gbfs;
 	private Graph grafoVazio;
 	private Graph grafoUmVertice;
 	private Graph grafoComPeso;
 	private Graph grafoPesoNegativo;
+	
 
-
-	//esse aqui é só um exemplo de teste, pode comecar após esse
 	@Test
 	public void test() {
 		try {
@@ -76,5 +84,89 @@ public class APITest {
 		assertEquals(GraphControl.BFS(grafoPesoNegativo, new Vertice(2)), saidaPesoNegativo);
 
 	}
+	
+	@Test
+	public void testConnected2() throws Exception {
+		grafoTeste1 = GraphControl.readGraph("graph1.txt");
+		grafoTeste2 = GraphControl.readGraph("graph2.txt");
+		grafoTeste3 = GraphControl.readGraph("graph3.txt");
+ 	    grafoTeste4 = GraphControl.readGraph("graph4.txt");
+		grafoTeste5 = GraphControl.readGraph("graph5.txt");
+		grafoTeste6 = GraphControl.readGraph("graph6.txt");
+		grafoTeste7 = GraphControl.readGraph("graph7.txt");
+
+
+		assertFalse(GraphControl.connected(grafoTeste1));
+		assertTrue(GraphControl.connected(grafoTeste2));
+		assertTrue(GraphControl.connected(grafoTeste3));
+		assertTrue(GraphControl.connected(grafoTeste4));
+		assertTrue(GraphControl.connected(grafoTeste5));
+		assertTrue(GraphControl.connected(grafoTeste6));
+		assertTrue(GraphControl.connected(grafoTeste7));
+
+	}
+	@Test
+	public void testVertexNumber() throws Exception {
+		grafoTeste7 = GraphControl.readGraph("graph7.txt");
+		grafoTeste1 = GraphControl.readGraph("graph1.txt");
+		grafoTeste2 = GraphControl.readGraph("graph2.txt");
+		grafoTeste3 = GraphControl.readGraph("graph3.txt");
+ 	    grafoTeste4 = GraphControl.readGraph("graph4.txt");
+		grafoTeste5 = GraphControl.readGraph("graph5.txt");
+		grafoTeste6 = GraphControl.readGraph("graph6.txt");
+		
+		assertEquals(4, GraphControl.getVertexNumber(grafoTeste1));
+		assertEquals(3, GraphControl.getVertexNumber(grafoTeste2));
+		assertEquals(4, GraphControl.getVertexNumber(grafoTeste3));
+		assertEquals(4, GraphControl.getVertexNumber(grafoTeste4));
+		assertEquals(18, GraphControl.getVertexNumber(grafoTeste5));
+		assertEquals(7, GraphControl.getVertexNumber(grafoTeste6));
+		assertEquals(24, GraphControl.getVertexNumber(grafoTeste7));
+		
+	}
+	
+	@Test
+	public void testEdgeNumber() throws Exception {
+		grafoTeste7 = GraphControl.readGraph("graph7.txt");
+		grafoTeste1 = GraphControl.readGraph("graph1.txt");
+		grafoTeste2 = GraphControl.readGraph("graph2.txt");
+		grafoTeste3 = GraphControl.readGraph("graph3.txt");
+ 	    grafoTeste4 = GraphControl.readGraph("graph4.txt");
+		grafoTeste5 = GraphControl.readGraph("graph5.txt");
+		grafoTeste6 = GraphControl.readGraph("graph6.txt");
+		
+
+		assertEquals(2, GraphControl.getEdgeNumber(grafoTeste1));
+		assertEquals(2, GraphControl.getEdgeNumber(grafoTeste2));
+		assertEquals(3, GraphControl.getEdgeNumber(grafoTeste3));
+		assertEquals(4, GraphControl.getEdgeNumber(grafoTeste4));
+		assertEquals(20, GraphControl.getEdgeNumber(grafoTeste5));
+		assertEquals(6, GraphControl.getEdgeNumber(grafoTeste6));
+		assertEquals(30, GraphControl.getEdgeNumber(grafoTeste7));
+		
+	}
+	
+	@Test
+	public void testMeanEdgeNumber() throws Exception {
+		
+		grafoTeste7 = GraphControl.readGraph("graph7.txt");
+		grafoTeste1 = GraphControl.readGraph("graph1.txt");
+		grafoTeste2 = GraphControl.readGraph("graph2.txt");
+		grafoTeste3 = GraphControl.readGraph("graph3.txt");
+ 	    grafoTeste4 = GraphControl.readGraph("graph4.txt");
+		grafoTeste5 = GraphControl.readGraph("graph5.txt");
+		grafoTeste6 = GraphControl.readGraph("graph6.txt");
+		
+
+		assertEquals(1, GraphControl.getEdgeNumber(grafoTeste1));
+		assertEquals(1.3, GraphControl.getEdgeNumber(grafoTeste2));
+		assertEquals(1.5, GraphControl.getEdgeNumber(grafoTeste3));
+		assertEquals(2, GraphControl.getEdgeNumber(grafoTeste4));
+		assertEquals(2.23, GraphControl.getEdgeNumber(grafoTeste5));
+		assertEquals(1.7, GraphControl.getEdgeNumber(grafoTeste6));
+		assertEquals(1.6, GraphControl.getEdgeNumber(grafoTeste7));
+		
+	}
+	
 
 }
